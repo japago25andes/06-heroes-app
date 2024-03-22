b8e921d9f2cc471c7f2edb20e339e3970bc74c03
japago25andes <j.pardog@uniandes.edu.co>
2024-03-22 11:00:48 -0500

    Esquema incial del proyecto

---
+++ src/app/auth/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+})
+export class LayoutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
